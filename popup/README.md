f4c3a880f4e0eef181266c230bdaa6870dea6bd6
philippG777 <philippG777@protonmail.com>
2020-01-12 18:22:19 +0100

    Initial commit

---
+++ popup/README.md
@@ -0,0 +1,9 @@
+# Robots
+
+A WebExtention for viewing robots.txt files.
+
+The following images are used by this web-extension:
+https://www.iconfinder.com/icons/3099458/3_face_robot_icon ,
+https://www.iconfinder.com/icons/2120162/astronaut_astronomy_robot_science_space_icon .
+
+Both are licensed under https://creativecommons.org/licenses/by/3.0/ .
---
+++ popup/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function log(data) {
+    console.log(data);
+}
+
+function showRobotsTxt(text) {
+    log(text);
+    const contentHolder = document.getElementById('content');
+    contentHolder.innerText = text;
+    contentHolder.style.display = 'block';
+}
+
+function getHost(url) {
+    //  doesn't work with file-url
+    if(url.indexOf('file://') == 0)
+        return;
+    
+    const parts = url.split('/');
+
+    for(const i in parts) {
+        const item = parts[i];
+        if(item != '' &&
+          (item.indexOf('.') != -1 || item.indexOf(':') != -1) &&
+          item.indexOf(':') != item.length - 1)
+            return parts[0] + '//' + item;     // return host with protocol
+    }
+    return;
+}
+
+function fetchRobotsTxt(url) {
+    const host = getHost(url); 
+    if(host != undefined)
+        fetch(host + '/robots.txt').then((res) => {
+            log(res);
+            if(res.status == 200)
+                res.text().then(showRobotsTxt);
+        });
+}
+
+browser.tabs.query({
+    active: true
+}).then((activeTabs) => {
+    for(const i in activeTabs)
+        if(activeTabs[i].url != undefined)
+            fetchRobotsTxt(activeTabs[i].url);
+});
